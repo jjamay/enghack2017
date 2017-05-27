67d1eb424f2832d625c962845e85a06edbb5e3da
Aaron He <aaronhe02@gmail.com>
2017-05-27 10:32:38 -0400

    api calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
 import SelectedFoods from './SelectedFoods';
 import FoodSearch from './FoodSearch';
+import axios from 'axios';
+import 'whatwg-fetch';
 
 class App extends Component {
   state = {
     selectedFoods: [],
-  }
-
-  removeFoodItem = (itemIndex) => {
-    const filteredFoods = this.state.selectedFoods.filter(
-      (item, idx) => itemIndex !== idx,
-    );
-    this.setState({ selectedFoods: filteredFoods });
+    newEvent : {
+      location: "rch",
+      roomNumber: "202",
+      startingTime: new Date(),
+      servingSize: 30,
+      foodType: "Pizza"
+    }
   }
 
   addFood = (food) => {
     const newFoods = this.state.selectedFoods.concat(food);
     this.setState({ selectedFoods: newFoods });
+  }
+
+  addEvent = () => {
+    // fetch('/api/addEvent', {
+    //   method: 'POST',
+    //   body: JSON.stringify({
+    //     location: "rch",
+    //     roomNumber: "202",
+    //     startingTime: new Date(),
+    //     servingSize: 30,
+    //     foodType: "Pizza"
+    //   })
+    // }).then((response) => {
+    //   console.log(response);
+    // });
+    axios.post('/api/addEvent', {
+        location: "e5",
+        roomNumber: "6008",
+        startingTime: (new Date()).getTime()-100000000,
+        servingSize: 30,
+        foodType: "Pizza"
+    }).then((response) => {
+        console.log(response);
+    });
+
+
+  }
+
+  upvote = () => {
+    axios.post('/api/upvote', {
+        id:"59298222cd57fa1988fc8c3b"
+    }).then((response) => {
+        console.log(response);
+    });
   }
 
   render() {
@@ -32,6 +68,9 @@ class App extends Component {
           <FoodSearch
             onFoodClick={this.addFood}
           />
+
+          <button onClick = {this.addEvent}>Create Event</button>
+          <button onClick = {this.upvote}>Upvote</button>
         </div>
       </div>
     );
